f379cb2f9c32d2d79fc0567165dd6d38d60a6f47
it25100957 <it25100957@my.sliit.lk>
2025-08-30 17:00:00 +0530

    Day 2 - added 3x3 board display

--- game.c
+++ game.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
-int main() 
-{
-    printf("=== TIC TAC TOE ===\n");
-    printf("Player 1 : X\nPlayer 2 : O\n");
-    return 0;
+int main() {
+char board[3][3];
+int i, j;
+
+// initialize empty spaces
+for(i = 0; i < 3; i++) {
+    for(j = 0; j < 3; j++) {
+         board[i][j] = ' ';
+    }
 }
 
+// display the board
+printf("=== TIC TAC TOE ===\n\n");
+for(i = 0; i < 3; i++) {
+    for(j = 0; j < 3; j++) {
+        printf(" %c ", board[i][j]);
+    }
+    printf("\n");
+}
+
+return 0;
+}
