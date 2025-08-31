34bacb020907617106b065e68d21b7c96a8dfb2b
it25100957 <it25100957@my.sliit.lk>
2025-08-31 21:44:12 +0530

    Day 3 - player 1 can place X

--- game.c
+++ game.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 
 int main() {
-char board[3][3];
+
+char board[3][3] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
+int row, col;
 int i, j;
 
-// initialize empty spaces
-for(i = 0; i < 3; i++) {
-    for(j = 0; j < 3; j++) {
-         board[i][j] = ' ';
-    }
-}
+printf("=== TIC TAC TOE ===\n");
+printf("Player 1 uses X\n");
+printf("Enter row (space) column, ((row,column = 0, 1, 2)):  ");
 
-// display the board
-printf("=== TIC TAC TOE ===\n\n");
+// take input for X
+scanf("%d %d", &row, &col);
+
+// place X on the board
+board[row][col] = 'X';
+
+// show updated board
+printf("\nCurrent board:\n");
 for(i = 0; i < 3; i++) {
     for(j = 0; j < 3; j++) {
-        printf(" %c ", board[i][j]);
+        printf("|%c", board[i][j]);
     }
-    printf("\n");
+    printf("|\n");
 }
 
 return 0;
 }
+
+
+
