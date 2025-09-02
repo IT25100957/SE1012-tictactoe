c98495817eceab05ff5458297e90285c8fcbd6cf
it25100957 <it25100957@my.sliit.lk>
2025-09-02 21:38:42 +0530

    Day 5 - added row win check (underscore naming)

--- game.c
+++ game.c
@@ -1,46 +1,57 @@
 #include <stdio.h>
 
-int main() {
+int check_row(char grid[3][3]) {
+    for (int row_i = 0; row_i < 3; row_i++) {
+        if (grid[row_i][0] == grid[row_i][1] &&
+            grid[row_i][1] == grid[row_i][2] &&
+            grid[row_i][0] != ' ')
+            return 1;     // a row has the same mark
+    }
+    return 0;
+}
 
+int main() {
 char board[3][3] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
-int turn = 1;      // 1 = Player X, 2 = Player O
-int moves = 0;
-int row, col;
-int i, j;
+int player_turn = 1;
+int move_count = 0;
+int row_no, col_no;
+int row_i, col_i;
+int game_win = 0;
 
 printf("=== TIC TAC TOE ===\n");
-printf("Enter row (space) column, ((row,column = 0, 1, 2))\n");
-printf("Player 1 (X) and Player 2 (O)\n");
 
-while(moves < 9) {
-    printf("\nPlayer %d enter row and column (0-2): ", turn);
-    scanf("%d %d", &row, &col);
+    while (move_count < 9 && !game_win) {
+        printf("\nPlayer %d enter row and column (0-2): ", player_turn);
+        scanf("%d %d", &row_no, &col_no);
 
-    // only place mark if the cell is empty
-    if(board[row][col] == ' ') {
-        if(turn == 1)
-            board[row][col] = 'X';
-        else
-            board[row][col] = 'O';
+    if (board[row_no][col_no] == ' ') {
+        board[row_no][col_no] = (player_turn == 1) ? 'X' : 'O';
+        move_count++;
 
-        moves++;
-        // switch turn
-        turn = (turn == 1) ? 2 : 1;
+        if (check_row(board)) {
+            printf("\nPlayer %d wins by row!\n", player_turn);
+            game_win = 1;
+        } else {
+            player_turn = (player_turn == 1) ? 2 : 1;
+        }
     } else {
-        printf("That cell is already used. Try again!\n");
+        printf("That cell is already filled. Try again!\n");
     }
 
-    // display board after every move
-    printf("\n");
-    for(i = 0; i < 3; i++) {
-        for(j = 0; j < 3; j++)
-            printf("|%c", board[i][j]);
+    // print board after each move
+    for (row_i = 0; row_i < 3; row_i++) {
+        for (col_i = 0; col_i < 3; col_i++)
+            printf("|%c", board[row_i][col_i]);
         printf("|\n");
+        }
     }
+
+    if (!game_win && move_count == 9)
+        printf("\nGame draw!\n");
+
+    return 0;
 }
 
-return 0;
-}
 
 
 
