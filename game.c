70cf14ba6e276ae36f4ec96d2a8b1dbdba134e98
it25100957 <it25100957@my.sliit.lk>
2025-09-04 21:54:37 +0530

    Day 7 - added draw check and function prototypes above main

--- game.c
+++ game.c
@@ -1,10 +1,56 @@
 #include <stdio.h>
 
-// checks all winning conditions (rows, columns, diagonals)
+// ====== Function Prototypes ======
+int check_win(char box[3][3]);
+void display_board(char grid[3][3]);
+int is_draw(int move_count, int game_win);
+
+// =================================
+int main() {
+    char board[3][3] = {
+        {' ',' ',' '},
+        {' ',' ',' '},
+        {' ',' ',' '}
+    };
+    int player_turn = 1;
+    int move_count = 0;
+    int row_no, col_no;
+    int game_win = 0;
+
+    printf("=== TIC TAC TOE ===\n");
+
+    while (move_count < 9 && !game_win) {
+        printf("\nPlayer %d enter row and column (0-2): ", player_turn);
+        scanf("%d %d", &row_no, &col_no);
+
+        if (board[row_no][col_no] == ' ') {
+            board[row_no][col_no] = (player_turn == 1) ? 'X' : 'O';
+            move_count++;
+
+            display_board(board);
+
+            if (check_win(board)) {
+                printf("\nPlayer %d wins!\n", player_turn);
+                game_win = 1;
+            } else if (is_draw(move_count, game_win)) {
+                printf("\nGame draw! No more moves left.\n");
+                break;
+            } else {
+                player_turn = (player_turn == 1) ? 2 : 1;
+            }
+        } else {
+            printf("That cell is already filled. Try again!\n");
+        }
+    }
+
+    printf("\n=== Game Over ===\n");
+    return 0;
+}
+
 int check_win(char box[3][3]) {
     int row_i, col_i;
 
-    // check each row
+    // check rows
     for (row_i = 0; row_i < 3; row_i++) {
         if (box[row_i][0] == box[row_i][1] &&
             box[row_i][1] == box[row_i][2] &&
@@ -12,7 +58,7 @@ int check_win(char box[3][3]) {
             return 1;
     }
 
-    // check each column
+    // check columns
     for (col_i = 0; col_i < 3; col_i++) {
         if (box[0][col_i] == box[1][col_i] &&
             box[1][col_i] == box[2][col_i] &&
@@ -20,62 +66,37 @@ int check_win(char box[3][3]) {
             return 1;
     }
 
-    // check main diagonal
+    // check diagonals
     if (box[0][0] == box[1][1] &&
         box[1][1] == box[2][2] &&
         box[0][0] != ' ')
         return 1;
 
-    // check anti-diagonal
     if (box[0][2] == box[1][1] &&
         box[1][1] == box[2][0] &&
         box[0][2] != ' ')
         return 1;
 
-    return 0; // no win yet
-}
-
-int main() {
-char board[3][3] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
-int player_turn = 1;
-int move_count = 0;
-int row_no, col_no;
-int row_i, col_i;
-int game_win = 0;
-
-printf("=== TIC TAC TOE ===\n");
-
-    while (move_count < 9 && !game_win) {
-        printf("\nPlayer %d enter row and column (0-2): ", player_turn);
-        scanf("%d %d", &row_no, &col_no);
-
-    if (board[row_no][col_no] == ' ') {
-        board[row_no][col_no] = (player_turn == 1) ? 'X' : 'O';
-        move_count++;
-
-        if (check_win(board)) {
-            printf("\nPlayer %d wins!\n", player_turn);
-            game_win = 1;
-        } else {
-            player_turn = (player_turn == 1) ? 2 : 1;
-        }
-    } else {
-        printf("That cell is already filled. Try again!\n");
-    }
-
-    // print board after each move
-    for (row_i = 0; row_i < 3; row_i++) {
-        for (col_i = 0; col_i < 3; col_i++)
-            printf("|%c", board[row_i][col_i]);
-        printf("|\n");
-        }
-    }
-
-    if (!game_win && move_count == 9)
-        printf("\nGame draw!\n");
-
     return 0;
 }
+
+void display_board(char grid[3][3]) {
+    int row_i, col_i;
+    printf("\nCurrent board:\n");
+    for (row_i = 0; row_i < 3; row_i++) {
+        for (col_i = 0; col_i < 3; col_i++)
+            printf("|%c", grid[row_i][col_i]);
+        printf("|\n");
+    }
+}
+
+int is_draw(int move_count, int game_win) {
+    if (!game_win && move_count == 9)
+        return 1;
+    return 0;
+}
+
+
 
 
 
